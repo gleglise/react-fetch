d0975b803d359e94cdd47e78741e2cd30d103b98
Geoffrey <geoffreyleglise@gmail.com>
2024-10-29 21:36:18 +0100

    button employee

---
+++ src/components/EmployeeCard.tsx
@@ -0,0 +1,27 @@
+// src/components/EmployeeCard.tsx
+interface EmployeeCardProps {
+    employee: {
+      name: {
+        first: string;
+        last: string;
+      };
+      email: string;
+      picture: {
+        medium: string;
+      };
+    };
+  }
+   
+  function EmployeeCard({ employee }: EmployeeCardProps) {
+    return (
+      <figure className='DisplayEmployee'>
+        <img src={employee.picture.medium} alt={employee.name.first} />
+        <figcaption>
+          <strong>{employee.name.first} {employee.name.last}</strong>
+          &nbsp;{employee.email}
+        </figcaption>
+      </figure>
+    );
+  }
+  
+  export default EmployeeCard;
